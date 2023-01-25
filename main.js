a9004876336f68b771b0a7bdc673f926cce73c25
IGOR ABUD FORTES <igorfortes@ciandt.com>
2023-01-25 13:28:41 -0300

    inicializando

---
+++ main.js
@@ -0,0 +1,36 @@
+function onScroll () {
+    showNavOnScroll();
+    showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll () {
+    scrollY > 0 ? navigation.classList.add('scroll') : navigation.classList.remove('scroll');
+}
+
+function showBackToTopButtonOnScroll () {
+    scrollY > 500 ? backToTopButton.classList.add('show') : backToTopButton.classList.remove('show');
+}
+
+function openMenu () {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu () {
+    document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+    origin: 'top', 
+    distance:'30px', 
+    duration: 700
+}).reveal(`#home, 
+           #home img, 
+           #home .stats, 
+           #services,
+           #services header,
+           #services .card,
+           #about,
+           #about header,
+           #about .content,
+           #contact,
+           #contact .content`);
